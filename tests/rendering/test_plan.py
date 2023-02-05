724a17d2cb859f976844a4a580cd3ef796f06aff
Riley Flynn <riley@rileyflynn.me>
2023-02-04 20:43:17 -0330

    Test RenderPlan.render

--- tests/rendering/test_plan.py
+++ tests/rendering/test_plan.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
-from spotify_cover_maker.models.covers import GradientCover
+import yaml
+from pytest import MonkeyPatch
+
+from spotify_cover_maker.models.covers import CoverFile, GradientCover
+from spotify_cover_maker.models.state import GeneratedCoverState, StateFile
 from spotify_cover_maker.rendering import PlanMode, RenderPlan
 
 test_path = Path(__file__).parent / "plan_test"
@@ -40,3 +44,32 @@ def test_plan_mode_missing() -> None:
         GradientCover(name="new", template="gradient"),
         GradientCover(name="missing_file", template="gradient"),
     ]
+
+
+def test_plan_render(tmp_path: Path, monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.chdir(tmp_path)
+
+    test_cover = GradientCover(name="test", template="gradient")
+
+    with open("covers.yaml", "w") as f:
+        yaml.safe_dump(
+            CoverFile(covers=[test_cover]).dict(),
+            f,
+        )
+
+    plan = RenderPlan(PlanMode.all, Path("covers.yaml"), Path(".scm_state.yaml"))
+
+    assert plan.covers == [test_cover]
+
+    plan.render()
+
+    assert Path(".scm_state.yaml").exists()
+    assert Path("covers").is_dir()
+    assert Path("covers", "test.png").is_file()
+
+    with open(".scm_state.yaml") as f:
+        state = StateFile.parse_obj(yaml.safe_load(f))
+
+        assert state == StateFile(
+            generated_covers={"test": GeneratedCoverState.for_cover(test_cover)}
+        )
