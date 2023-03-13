59f506e41de99eca705997860e611a6a2ff94a94
Riley Flynn <riley@rileyflynn.me>
2023-03-13 20:12:47 -0230

    Initial layout

--- spotify_cover_maker/cli/ui/root.py
+++ spotify_cover_maker/cli/ui/root.py
@@ -1,5 +1,22 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.containers import Container, Grid
+from textual.widgets import Header, Placeholder
 
 
-class UIRoot(App):
-    pass
+class UIRoot(App[None]):
+    TITLE = "Spotify Cover Maker"
+    CSS = """
+    .app {
+        layout: grid;
+        grid-size: 2 1;
+        grid-columns: 1fr 3fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Container(
+            Placeholder("Sidebar"),
+            Placeholder("Editor"),
+            classes="app"
+        )
