ef093333d450ea39acb1ae4a9d8fdd2f92a818ff
Riley Flynn <riley@rileyflynn.me>
2023-03-18 21:16:21 -0230

    Make editor actually edit

--- spotify_cover_maker/cli/ui/editors/gradient.py
+++ spotify_cover_maker/cli/ui/editors/gradient.py
@@ -1,15 +1,40 @@
 from textual.app import ComposeResult
+from textual.containers import Container
 from textual.widget import Widget
-from textual.widgets import Markdown
+from textual.widgets import Input, Label
 
 from spotify_cover_maker.models.covers import GradientCover
 
 
 class GradientEditor(Widget):
-    def __init__(self, cover: GradientCover):
-        self.cover = cover
+    DEFAULT_CSS = """
+    .editor_container {
+        padding: 1;
+    }
 
+    .input_container {
+        height: 5;
+        width: 100%;
+        margin-bottom: 1;
+    }
+    """
+
+    def __init__(self, cover: GradientCover):
         super().__init__()
 
+        self.cover = cover
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        if event.input.id == "title":
+            self.cover.title = event.value if event.value else None
+        elif event.input.id == "subtitle":
+            self.cover.subtitle = event.value if event.value else None
+
     def compose(self) -> ComposeResult:
-        yield Markdown(f"```json\n{self.cover.json(indent=2, sort_keys=True)}\n```")
+        with Container(classes="editor_container"):
+            with Container(classes="input_container"):
+                yield Label("Title")
+                yield Input(value=self.cover.title, id="title")
+            with Container(classes="input_container"):
+                yield Label("Subtitle")
+                yield Input(value=self.cover.subtitle, id="subtitle")
